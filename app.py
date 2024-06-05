fbf118db773215b668ac8a393b4cf53a136296e3
n-mitsui <petbookyear@yahoo.co.jp>
2024-06-06 04:04:30 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-import streamlit as st
-import pandas as pd
+# import streamlit as st
+# import pandas as pd
 
-import plotly.express as px
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+# import plotly.express as px
+# import plotly.graph_objects as go
+# from plotly.subplots import make_subplots
 
-from sklearn.datasets import load_iris
+# from sklearn.datasets import load_iris
 
 iris = load_iris()
 df = px.data.iris()
